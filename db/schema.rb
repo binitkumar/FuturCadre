b94d52a3dc1b0b3b4c3a802e57c76b3d6fd6b7cd
AttiqRao <attiq.rao@ilsainteractive.com>
2011-11-24 19:33:48 +0500

    added cancan and some GUI implementations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,25 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111123080526) do
+ActiveRecord::Schema.define(:version => 20111124134533) do
+
+  create_table "categories", :force => true do |t|
+    t.string   "name"
+    t.integer  "parent_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "roles", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "roles_users", :id => false, :force => true do |t|
+    t.integer "role_id"
+    t.integer "user_id"
+  end
 
   create_table "users", :force => true do |t|
     t.string   "email",                                 :default => "", :null => false
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+puts "Deleting existing roles..."
+roles = Role.all
+roles.each{ |role| role.destroy } if roles.present?
+
+puts "Creating roles..."
+%w(admin webmaster emploer job_seeker).each do |name|
+	Role.create!(:name => name)
+end
+
+puts "Deleting existing categorties..."
+categories = Category.all
+categories.each{ |category| category.destroy } if categories.present?
+
+puts "Creating categories..."
+%w(Accounting/Finance Administrative/Clerical Business/Strategic_Management Building Construction/Skilled_Trades Creative/Design
+Customer_Support/Client_Care Editorial/Writing Engineering Food_Services/Hospitality Human_Resources Installation/Maintenance/Repair
+IT/Software_Development Legal Logistics/Transportation Marketing/Product Medical/Health Production/Operations Project/Program_Management
+Quality_Assurance/Safety R_D/Science Sales/Business_Development Security/Protective_Services Training/Instruction Other).each do |name|
+	Category.create!(:name => name)
+end
