d835db3d8d585b50f3f0c4530eb0713922146c62
AttiqRao <attiq.rao@ilsainteractive.com>
2011-11-29 18:32:22 +0500

    fixed...

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,6 +39,6 @@ FuturCadre::Application.configure do
   # Expands the lines which load the assets
   config.assets.debug = true
 
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { :host => 'ilsainteractive:3001' }
   
 end
