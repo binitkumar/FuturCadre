a8bc90d98cc4f37ee8ff4f28d588df3e5cba6148
Ammar Rana <ammar.rana@ilsainteractive.com>
2011-12-07 21:48:31 +0500

    Still working on this portion of Profile form

--- app/assets/javascripts/profiles.js.coffee
+++ app/assets/javascripts/profiles.js.coffee
@@ -1,3 +1,10 @@
 # Place all the behaviors and hooks related to the matching controller here.
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/
+
+jQuery ->
+   $("#update_profile_button").click ->
+    $.post $("form#create_profile_form").attr("action"), $("form#create_profile_form").serialize(), ((response) ->
+      $(".content-holder").html response.html
+    ), "json"
+    false
--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -8,10 +8,22 @@ class ProfilesController < ApplicationController
 
 	def new
     @profile = Profile.new
-	end
+    @education_info = @profile.education_informtions
+    @job_info = @profile.profession_informtions
 
-	def create
+ 	end
 
-	end
+  def create_profile
+
+    @profile_new = Profile.create(:user_id =>current_user.id, :country_id => params[:country_id], :region_id=>params[:region_id],
+                   :city_id => params[:city_id], :first_name => params[:profile][:first_name], :last_name =>params[:profile][:last_name],
+                   :address => params[:profile][:address], :phone => params[:profile][:phone])
+
+   #educational information
+   unless @profile_new.blank?
+       @education_info = EducationInformation.create(:profile_id => @profile_new.id)
+   end
+    render :text => "ok"
+  end
 
 end
