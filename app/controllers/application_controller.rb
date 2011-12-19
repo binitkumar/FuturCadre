55d91da3d8590cf15c3dfff99191bee10b736390
SalmanNasir <salman@ilsainteractive.com>
2011-12-19 11:35:48 +0500

    added  i18n

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+	before_filter :set_locale
 
   def after_sign_in_path_for(resource_or_scope)
     current_user.webmaster? ? dashboard_admin_home_index_path : "/#{current_user.role}/dashboard"
@@ -8,5 +9,11 @@ class ApplicationController < ActionController::Base
   def after_sign_out_path_for(resource_or_scope)
     $role.present? && $role == "webmaster" ? login_admin_home_index_path : root_path
   end
+
+	private
+
+	def set_locale
+		@locale = I18n.locale = session[:locale] || I18n.default_locale
+	end
 
 end
