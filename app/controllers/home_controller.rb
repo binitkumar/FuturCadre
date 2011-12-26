7ab71238abddbb197f1131b8cda34b9570994117
Ammar Rana <ammar.rana@ilsainteractive.com>
2011-12-26 11:19:13 +0500

    featured group

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,7 @@ class HomeController < ApplicationController
 	def index
 		@categories = Category.main_categories
 		@projects = Project.all
+    @groups = Group.where("featured =? ",  true)
 	end
 
 	def change_laguage
