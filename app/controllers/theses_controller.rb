4ee0417e1f89885f5499d24cc73ea4d26c4abddc
Ammar Rana <ammar.rana@ilsainteractive.com>
2012-02-03 12:17:41 +0500

    fixed edit and show partial of thesis module for the Employer and JobSeeker

--- app/controllers/theses_controller.rb
+++ app/controllers/theses_controller.rb
@@ -1,146 +1,121 @@
 class ThesesController < ApplicationController
-  # GET /theses
-  # GET /theses.json
+
   def index
     unless params[:id]!=nil
       @category = Category.first
-      @theses = Thesis.all
+      @theses   = Thesis.all
     else
       @category = Category.find_by_id(params[:id])
-      @theses = @category.theses
+      @theses   = @category.theses
     end
     @categories = Category.all
-    respond_to do |format|
-      format.html # index.html.erb
-    end
+
   end
 
   def my_theses
-    @user = User.find_by_id(params[:id])
+    @user      = User.find_by_id(params[:id])
     @my_theses = User.theses
-    render :json => {:html => render_to_string(:partial => 'my_theses')}.to_json
+    render :json => { :html => render_to_string(:partial => 'my_theses') }.to_json
   end
 
-  # GET /theses/1
-  # GET /theses/1.json
+
   def show
     @thesis = Thesis.find(params[:id])
-    respond_to do |format|
-      format.html # show.html.erb
-    end
+
   end
 
-  # GET /theses/new
-  # GET /theses/new.json
+
   def new
-    @thesis = Thesis.new
+    @thesis     = Thesis.new
     @thesis_doc = Photo.new(params[:thesis_doc])
 
-    respond_to do |format|
-      format.html # new.html.erb
-    end
   end
 
-  # GET /theses/1/edit
+
   def edit
     @thesis = Thesis.find(params[:id])
   end
 
-  # POST /theses
-  # POST /theses.json
+
   def create_thesis
     @thesis = Thesis.new(params[:thesis])
     @thesis.update_attributes(:owner => current_user)
     unless params[:thesis_doc].blank?
-      @thesis_pub = Photo.new(params[:thesis_doc])
+      @thesis_pub              = Photo.new(params[:thesis_doc])
       @thesis_pub.content_type = "thesis_publication"
-      @thesis_pub.imageable = @thesis
+      @thesis_pub.imageable    = @thesis
+      @thesis_pub.save
+    end
+    if @thesis.save
+      render :json => { :html => render_to_string(:partial => 'show_theses', :locale=>{ :thesis => @thesis }) }.to_json
+    else
+      redirect_to(:action => "new", :notice => "Thesis was not created")
+    end
+  end
+
+
+  def update
+    @thesis = Thesis.find(params[:id])
+    unless params[:thesis_doc].blank?
+      @thesis_pub              = Photo.new(params[:thesis_doc])
+      @thesis_pub.content_type = "thesis_publication"
+      @thesis_pub.imageable    = @thesis
       @thesis_pub.save
     end
 
-    respond_to do |format|
-      if @thesis.save
-        redirect_to(@thesis, :notice => 'Thesis was successfully created.')
+    if @thesis.update_attributes(params[:thesis])
 
-      else
-        redirect_to(:action => "new", :notice => "Thesis was not created")
+      redirect_to :action => "index"
+    else
+      render :json => { :html => render_to_string(:partial => 'show_theses', :locale=>{ :thesis => @thesis  }) }.to_json
 
-      end
     end
+
   end
 
-  # PUT /theses/1
-  # PUT /theses/1.json
-  def update
-    @thesis = Thesis.find(params[:id])
-
-    respond_to do |format|
-      if @thesis.update_attributes(params[:thesis])
-
-
-      else
-
-
-      end
-    end
-  end
-
-  # DELETE /theses/1
-  # DELETE /theses/1.json
-  #def destroy
-  #  @thesis = Thesis.find(params[:id])
-  #  @thesis.destroy
-  #
-  #  respond_to do |format|
-  #    format.html { redirect_to theses_url }
-  #    format.json { head :ok }
-  #  end
-  #end
 
   def download_thesis
-    @thesis = Thesis.find_by_id(params[:id])
+    @thesis     = Thesis.find_by_id(params[:id])
     @thesis_doc = @thesis.photo.image
     send_file @thesis_doc.path
   end
 
   def thesis_details
 
-    @thesis = Thesis.find(params[:id])
+    @thesis     = Thesis.find(params[:id])
     @categories = Category.all
-    @comments = @thesis.comments.all
+    @comments   = @thesis.comments.all
   end
 
   def thesis_wall
-    @thesis = Thesis.find(params[:id])
-    @comment = @thesis.comments.build
+    @thesis   = Thesis.find(params[:id])
+    @comment  = @thesis.comments.build
     @comments = @thesis.comments.all
-    render :json => {:html => render_to_string(:partial => '/theses/thesis_wall', :locale=>{:thesis => @thesis})}.to_json
+    render :json => { :html => render_to_string(:partial => '/theses/thesis_wall', :locale=>{ :thesis => @thesis }) }.to_json
   end
 
   def add_comment
     @thesis = Thesis.find(params[:thesis_id])
     @thesis.comments.create(params[:comment])
     @comments = @thesis.comments.all
-    render :json => {:html => render_to_string(:partial => '/theses/thesis_wall', :locale=>{:thesis => @thesis})}.to_json
+    render :json => { :html => render_to_string(:partial => '/theses/thesis_wall', :locale=>{ :thesis => @thesis }) }.to_json
   end
 
   def thesis_body
     @thesis = Thesis.find(params[:id])
-    render :json => {:html => render_to_string(:partial => '/theses/thesis_path', :locale=>{:thesis => @thesis})}.to_json
+    render :json => { :html => render_to_string(:partial => '/theses/thesis_path', :locale=>{ :thesis => @thesis }) }.to_json
   end
 
   def thesis_category
     @category = Category.find_by_id(params[:id])
-    @theses = @category.theses
-    render :json => {:html => render_to_string(:partial => '/theses/thesis_categories', :locale=>{:theses => @theses})}.to_json
+    @theses   = @category.theses
+    render :json => { :html => render_to_string(:partial => '/theses/thesis_categories', :locale=>{ :theses => @theses }) }.to_json
   end
 
 
   def search_thesis
     @thesis_result= Thesis.find_all_by_name(params[:search_term])
-    render :json => {:html => render_to_string(:partial => '/theses/search_result')}.to_json
-
-
+    render :json => { :html => render_to_string(:partial => '/theses/search_result') }.to_json
   end
 
 end
