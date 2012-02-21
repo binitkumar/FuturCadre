5bcc25430a488d3d7f4d2754597910b51cbf43a7
Ammar Rana <ammar.rana@ilsainteractive.com>
2012-02-21 20:36:39 +0500

    fixed controller json

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,57 +1,38 @@
 class NewsController < ApplicationController
-  # GET /news
-  # GET /news.json
+
   def index
     @news = News.all(:conditions => { :is_approved => true })
 
   end
 
-  # GET /news/1
-  # GET /news/1.json
   def show
     @news = News.find(params[:id])
   end
 
-  # GET /news/new
-  # GET /news/new.json
   def new
     @news = News.new
   end
 
-  # GET /news/1/edit
+
   def edit
     @news = News.find(params[:id])
   end
 
-  # POST /news
-  # POST /news.json
-  def create
-    @news = News.new(params[:news])
-    if @news.save
-      format.html { redirect_to @news, notice: 'News was successfully created.' }
-    else
-      format.html { render action: "new" }
-    end
 
-  end
 
-  # PUT /news/1
-  # PUT /news/1.json
-  def update
-    @news = News.find(params[:id])
-    if @news.update_attributes(params[:news])
-      format.html { redirect_to @news, notice: 'News was successfully updated.' }
-    else
-      format.html { render action: "edit" }
-    end
-  end
+  #def update
+  #  @news = News.find(params[:id])
+  #  if @news.update_attributes(params[:news])
+  #    format.html { redirect_to @news, notice: 'News was successfully updated.' }
+  #  else
+  #    format.html { render action: "edit" }
+  #  end
+  #end
 
-  # DELETE /news/1
-  # DELETE /news/1.json
   def destroy
     @news = News.find(params[:id])
     @news.destroy
-    format.html { redirect_to news_index_url }
+
 
   end
 
