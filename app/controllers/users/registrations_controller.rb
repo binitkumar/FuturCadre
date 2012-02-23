88b6279c657bc2556a2d8940ea8be49e6591f584
Ammar Rana <ammar.rana@ilsainteractive.com>
2012-02-23 11:34:10 +0500

    fixed devise error for user registration

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,50 +1,40 @@
 class Users::RegistrationsController < Devise::RegistrationsController
 
   def new
-    @resource = User.new
-    if params[:role]=="job_seeker"
-
-      @role = Role.find_by_name(params[:role].to_s)
-    else
-
-      @role = Role.find_by_name(params[:role].to_s)
-    end
+    @role = Role.find_by_name("job_seeker")
+     resource = build_resource({})
 
   end
 
+
   def create
-    @resource = User.new
     build_resource
-     @role = Role.find_by_id(params[:role_id])
+    @role = Role.find_by_id(params[:role_id])
+    resource.roles << @role
 
-    @resource.roles << @role
-
-    if   simple_captcha_valid? && params[:terms] && @resource.save
-      if @resource.active_for_authentication?
+    if   simple_captcha_valid? && params[:terms] && resource.save
+      if resource.active_for_authentication?
         set_flash_message :notice, :signed_up if is_navigational_format?
-        sign_in(@resource_name, @resource)
-        respond_with @resource, :location => redirect_location(@resource_name, @resource)
+        sign_in(resource_name, resource)
+        respond_with resource, :location => redirect_location(resource_name, resource)
       else
-        set_flash_message :notice, :inactive_signed_up, :reason => inactive_reason(@resource) if is_navigational_format?
+        set_flash_message :notice, :inactive_signed_up, :reason => inactive_reason(resource) if is_navigational_format?
         expire_session_data_after_sign_in!
-        respond_with @resource, :location => after_inactive_sign_up_path_for(@resource)
+        respond_with resource, :location => after_inactive_sign_up_path_for(resource)
       end
     else
-
       if !simple_captcha_valid?
         set_flash_message :error, :missing_captcha
       elsif params[:terms]==nil
         set_flash_message :error, :missing_privacy
       end
-      respond_with_navigational(@resource) { render_with_scope :new }
+      respond_with_navigational(resource) { render_with_scope :new }
     end
   end
 
   def new_user
-
-    @role = Role.find_by_name(params[:role].to_s)
-    #render :json => { :html => render_to_string(:partial => 'new', :locals => { :role => @role }) }.to_json
-    render :action =>"new", :locals => { :role =>@role, :test => "test" }
+    @role = Role.find_by_name(params[:role_name].to_s)
+    resource = build_resource({})
+    render :json => { :html => render_to_string(:partial => 'form', :locals => { :role => @role }) }.to_json
   end
-
 end
--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -7,12 +7,12 @@ module DeviseHelper
   # it to add more behavior or options.
   def devise_error_messages!
 
-    return "" if @resource.errors.empty?
+    return "" if resource.errors.empty?
 
-    messages = @resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
+    messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
     sentence = I18n.t("errors.messages.not_saved",
-                      :count    => @resource.errors.count,
-                      :resource => @resource.class.model_name.human.downcase)
+                      :count    => resource.errors.count,
+                      :resource => resource.class.model_name.human.downcase)
 
     html = <<-HTML
     <div id="error_explanation">
