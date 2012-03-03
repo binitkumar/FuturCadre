0248457b97203268230c60d9029c1f7c31d3177e
AmmarRana <ammarrana@ammarrana-Inspiron-6000.(none)>
2012-03-03 14:52:51 +0500

    fixed job application check on cv and non cv

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -8,7 +8,7 @@ class JobsController < ApplicationController
   end
 
   def details
-    @job         = Job.find_by_id(params[:id])
+    @job = Job.find_by_id(params[:id])
     @applied_job = AppliedJob.new
     if user_signed_in?
       if current_user.profile !nil
@@ -16,9 +16,9 @@ class JobsController < ApplicationController
       end
     end
     if params[:sid]=="group"
-      render :json => { :html => render_to_string(:partial => '/jobs/details') }.to_json
+      render :json => {:html => render_to_string(:partial => '/jobs/details')}.to_json
     else
-      render :json => { :html => render_to_string(:partial => '/jobs/details') }.to_json
+      render :json => {:html => render_to_string(:partial => '/jobs/details')}.to_json
     end
   end
 
@@ -26,10 +26,10 @@ class JobsController < ApplicationController
   def new
 
     if current_user.profile!=nil
-      @job            = Job.new
-      @skill          = Skill.new(params[:skills])
+      @job = Job.new
+      @skill = Skill.new(params[:skills])
       @responsibility = Responsibility.new(params[:respon])
-      @employer       = current_user
+      @employer = current_user
       unless @employer.profile.blank?
         @company_information = @employer.profile.company_information
       else
@@ -96,7 +96,7 @@ class JobsController < ApplicationController
       @job_languages.each_with_index do |job_lang, j|
         job_lang.update_attributes(:level=> params[:level][j])
       end
-      render :json => { :html => render_to_string(:partial => 'job_show', :locale=>{ :job_new => @job_new }) }.to_json
+      render :json => {:html => render_to_string(:partial => 'job_show', :locale=>{:job_new => @job_new})}.to_json
     else
       redirect_to :action => "new"
     end
@@ -105,16 +105,16 @@ class JobsController < ApplicationController
 
 
   def edit
-    @job                 = Job.find_by_id(params[:id])
+    @job = Job.find_by_id(params[:id])
     @company_information = @job.employer.profile.company_information
-    @skills              = @job.skills
-    @job_languages       = @job.languages
-    @job_educations      = @job.education_levels
+    @skills = @job.skills
+    @job_languages = @job.languages
+    @job_educations = @job.education_levels
 
   end
 
   def update
-    @job_new  = Job.find(params[:id])
+    @job_new = Job.find(params[:id])
     @job_comp = CompanyInformation.find(params[:cid])
 
     unless params[:job].blank?
@@ -165,12 +165,12 @@ class JobsController < ApplicationController
     @job_languages.each_with_index do |job_lang, j|
       job_lang.update_attributes(:level_id => params[:level][j])
     end
-    render :json => { :html => render_to_string(:partial => 'job_show', :locale=>{ :job_new => @job_new }) }.to_json
+    render :json => {:html => render_to_string(:partial => 'job_show', :locale=>{:job_new => @job_new})}.to_json
 
   end
 
   def remove_language
-    @job      = Job.find_by_id(params[:job_id])
+    @job = Job.find_by_id(params[:job_id])
     @language = Language.find_by_id(params[:lang_id])
     @job.languages.destroy(@language.id)
     render :nothing => true
@@ -183,27 +183,35 @@ class JobsController < ApplicationController
   end
 
   def new_application
-    @job         = Job.find(params[:id])
+    @job = Job.find(params[:id])
     @applied_job = AppliedJob.new
-    if user_signed_in?
-      if current_user.profile !nil
-        @cvs =current_user.profile.assets.where(:content_type => 'cv', :is_publishable => true, :is_deleted => false)
-      end
+
+    if current_user.profile !nil
+      @cvs =current_user.profile.assets.where(:content_type => 'cv', :is_publishable => true, :is_deleted => false)
     end
-    render :json => { :html => render_to_string(:partial => 'application_form', :locale=>{ :job_new => @job_new }) }.to_json
+
+    render :json => {:html => render_to_string(:partial => 'application_form', :locale=>{:job_new => @job_new})}.to_json
 
   end
 
   def apply_job
 
-    @job_seeker      = current_user.id
-    @job             = Job.find(params[:job_id])
-    @cv              = Asset.find(params[:cv_id])
+    @job = Job.find(params[:job_id])
+    unless params[:cv_id].blank?
+      @cv = Asset.find(params[:cv_id])
+    end
     @job_application = AppliedJob.new
-    @job_application.update_attributes(:user_id => @job_seeker, :job_id => @job.id, :cv_id => @cv.id)
+    @job_application.user_id = current_user.id
+    @job_application.job_id = @job.id
+    unless params[:cv_id].blank?
+      @cv = Asset.find(params[:cv_id])
+      @job_application.cv_id = @cv.id
+    end
+
+
     if @job_application.save
       #render :json => {:html => render_to_string(:partial => '/job_seeker/job_list', :locale=>{:job_seeker => @job_seeker})}.to_json
-      render :json => { :html => render_to_string(:partial => '/job_seeker/job_list', :locale=>{ :job_seeker => @job_seeker }) }.to_json
+      render :json => {:html => render_to_string(:partial => '/job_seeker/job_list', :locale=>{:job_seeker => @job_seeker})}.to_json
 
     else
       redirect_to profiles_path(), :notice => "Please creater a profile"
@@ -211,7 +219,7 @@ class JobsController < ApplicationController
   end
 
   def remove_education
-    @job       = Job.find_by_id(params[:job_id])
+    @job = Job.find_by_id(params[:job_id])
     @education = EducationLevel.find_by_id(params[:ed_id])
     @job.education_levels.destroy(@education.id)
     render :nothing => true
