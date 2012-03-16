6bcfb13dda6f9a3a7752944826c8d36ead3c5769
Ammar Rana <ammar.rana@ilsainteractive.com>
2012-03-16 20:00:12 +0500

    fixed Job_Seeker profile bugs

--- db/migrate/20111201112517_create_profession_informations.rb
+++ db/migrate/20111201112517_create_profession_informations.rb
@@ -1,23 +1,24 @@
 class CreateProfessionInformations < ActiveRecord::Migration
-  def up
+  #def up
+  def change
     create_table :profession_informations do |t|
       t.integer :profile_id
       t.string :profession_industry
-			t.string :profession_experience
-			#t.integer :category_id
+      t.string :profession_experience
+      #t.integer :category_id
       #t.integer :sector_id
       #t.string :career_level
-			t.string :job_title
-			t.string :company_name
-      t.date:start_date
-      t.date:end_date
+      t.string :job_title
+      t.string :company_name
+      t.datetime :start_date
+      t.datetime :end_date
       t.references :category, :sector
-
-			
       t.timestamps
     end
-  end
-  def down
-    drop_table :profession_informations
+    #end
+
+    #def down
+    #  drop_table :profession_informations
+    #end
   end
 end
