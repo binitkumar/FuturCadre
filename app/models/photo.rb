2b7f3b2a70db708ae90ee9a64a3de44d35aa227f
Ammar Rana <ammar.rana@ilsainteractive.com>
2012-03-27 20:53:25 +0500

    fixed issues with Paperclip attachment

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,12 +1,14 @@
 class Photo < ActiveRecord::Base
   belongs_to :imageable, :polymorphic => true
 
-  has_attached_file :image, :styles => { :small => "100x100>" }, :path => ":rails_root/public/system/:attachment/:id/:style/:basename.:extension",
+  has_attached_file :image,  :path => ":rails_root/public/system/:attachment/:id/:style/:basename.:extension",
                     :url            => "/system/:attachment/:id/:style/:basename.:extension"
   validates_attachment_content_type :image, :content_type=>['image/jpeg', 'image/png', 'image/gif', 'application/msword', 'application/pdf', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document']
 
 
   validates_attachment_size :image, :less_than => 2.megabytes,
                             :unless            => Proc.new { |model| model.image }
+
+  #:styles => { :small => "100x100>" },
 
 end
