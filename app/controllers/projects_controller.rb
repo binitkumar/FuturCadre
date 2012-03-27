e036b2b8a88c2c7b9ae66576dc771b0431aaabc5
Adnan Ali Arshad <adnan@adnan.(none)>
2012-03-27 16:40:40 +0500

    projects controller fix

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,22 +2,20 @@ class ProjectsController < ApplicationController
   before_filter :authenticate_user!, :except => [:index, :show]
 
   def index
-    @projects = Project.all
-
-
+    @projects = Project.find(:all, :conditions => {:is_deleted => false})
   end
 
 
   def show
-    @project       = Project.find(params[:id])
-    @project_users = ProjectUser.find_all_by_project_id(@project.id, :conditions => { :is_approved => true })
+    @project = Project.find(params[:id])
+    @project_users = ProjectUser.find_all_by_project_id(@project.id, :conditions => {:is_approved => true})
 
   end
 
 
   def new
     @project = Project.new(params[:project])
-    @photo   = Photo.new
+    @photo = Photo.new
   end
 
 
@@ -27,13 +25,13 @@ class ProjectsController < ApplicationController
 
 
   def create
-    @project       = Project.new(params[:project])
+    @project = Project.new(params[:project])
     @project.owner = current_user
     if @project.save
       unless params[:photo].blank?
-        @picture              = Photo.new(params[:photo])
+        @picture = Photo.new(params[:photo])
         @picture.content_type = "project_image"
-        @picture.imageable    = @project
+        @picture.imageable = @project
         unless @picture.save
           render :action => "new"
         end
@@ -64,24 +62,24 @@ class ProjectsController < ApplicationController
   def invite_users
 
     @project = Project.find_by_id(params[:id])
-    @users   = User.find_by_sql(" SELECT * FROM users u WHERE
+    @users = User.find_by_sql(" SELECT * FROM users u WHERE
          u.id NOT IN ( SELECT pu.user_id FROM projects p
         	INNER JOIN project_users pu ON
         	pu.project_id = p.id where p.id ='#{params[:id]}')
         	AND u.id != '#{current_user.id}' AND u.id != 1")
 
-    render :json => { :html => render_to_string(:partial => '/projects/invitation_form', :locale => { :project => @project }) }.to_json
+    render :json => {:html => render_to_string(:partial => '/projects/invitation_form', :locale => {:project => @project})}.to_json
   end
 
   def project_invitation
     if params[:add].to_i== 1
-      @project_user            = ProjectUser.new
-      @project_user.user_id    = params[:user_id]
+      @project_user = ProjectUser.new
+      @project_user.user_id = params[:user_id]
       @project_user.project_id = params[:id]
       @project_user.save
-      @project       = Project.find_by_id(params[:id])
+      @project = Project.find_by_id(params[:id])
       approve = invitation_response_projects_url(:id => @project_user.project.id, :user_id => @project_user.user.id, :cond => true)
-      reject  = invitation_response_projects_url(:id => @project_user.project.id, :user_id => @project_user.user.id, :cond => false)
+      reject = invitation_response_projects_url(:id => @project_user.project.id, :user_id => @project_user.user.id, :cond => false)
 
       current_user.send_message("Invited to Project", "Welcome to my project click to join Project'  #{@project.name}'  '#{@project.tag_line}'  '<a href='#{approve}'> approve</a> or '<a href='#{reject}'> reject</a>", @project_user.user)
       render :nothing => true
@@ -109,34 +107,34 @@ class ProjectsController < ApplicationController
   end
 
   def my_projects
-    @projects = Project.find_all_by_owner_id(current_user.id, :conditions => { :is_deleted => false })
+    @projects = Project.find_all_by_owner_id(current_user.id, :conditions => {:is_deleted => false})
     if current_user.job_seeker?
-      render :json => { :html => render_to_string(:partial => '/projects/my_projects', :locale => { :employer => current_user }) }.to_json
+      render :json => {:html => render_to_string(:partial => '/projects/my_projects', :locale => {:employer => current_user})}.to_json
     else
-      render :json => { :html => render_to_string(:partial => '/projects/my_projects', :locale => { :job_seeker => current_user }) }.to_json
+      render :json => {:html => render_to_string(:partial => '/projects/my_projects', :locale => {:job_seeker => current_user})}.to_json
     end
   end
 
   def delete_project
     @project = Project.find(params[:id])
     @project.update_attributes(:is_deleted => true)
-    @projects = Project.find_all_by_owner_id(current_user.id, :conditions => { :is_deleted => false })
+    @projects = Project.find_all_by_owner_id(current_user.id, :conditions => {:is_deleted => false})
     if current_user.job_seeker?
-      render :json => { :html => render_to_string(:partial => '/projects/my_projects', :locale => { :employer => current_user }) }.to_json
+      render :json => {:html => render_to_string(:partial => '/projects/my_projects', :locale => {:employer => current_user})}.to_json
     else
-      render :json => { :html => render_to_string(:partial => '/projects/my_projects', :locale => { :job_seeker => current_user }) }.to_json
+      render :json => {:html => render_to_string(:partial => '/projects/my_projects', :locale => {:job_seeker => current_user})}.to_json
     end
   end
 
   def project_request
-    @project_user            = ProjectUser.new
-    @project_user.user_id    = current_user.id
+    @project_user = ProjectUser.new
+    @project_user.user_id = current_user.id
     @project_user.project_id = params[:id]
     @project_user.save
-    @project       = Project.find_by_id(params[:id])
+    @project = Project.find_by_id(params[:id])
     @project_owner = @project.owner
-    approve        = invitation_response_projects_url(:id => @project_user.project.id, :user_id => @project_user.user.id, :cond => true)
-    reject         = invitation_response_projects_url(:id => @project_user.project.id, :user_id => @project_user.user.id, :cond => false)
+    approve = invitation_response_projects_url(:id => @project_user.project.id, :user_id => @project_user.user.id, :cond => true)
+    reject = invitation_response_projects_url(:id => @project_user.project.id, :user_id => @project_user.user.id, :cond => false)
     current_user.send_message("Request to join Project'#{@project.name}'", "'#{current_user.name}' has request to join project click to '<a href='#{approve}'> approve</a> or '<a href='#{reject}'> reject</a>", @project_owner)
     render :nothing => true
   end
