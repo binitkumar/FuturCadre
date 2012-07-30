1b87674b40a2d1943bf07c3fac737d9e5018e20b
Ammar Rana <ammarrana@nouman-desktop.(none)>
2012-07-30 20:48:23 +0500

    fixed patch

---
+++ config/initializers/mysql_patch.rb
@@ -0,0 +1,12 @@
+module ActiveRecord
+  module ConnectionAdapters
+    class MysqlAdapter
+    private
+      def connect_with_local_infile
+        @connection.options(Mysql::OPT_LOCAL_INFILE, 1)
+        connect_without_local_infile
+      end
+      alias_method_chain :connect, :local_infile
+    end
+  end
+end
